068633aaa6da8bd7a17df00423d7e4c26c17ca31
DJZ <1601156268@qq.com>
2020-11-17 17:41:06 +0800

    t

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.urls import path
-
-app_name = 'users'
-
-urlpatterns = [
-    path('register/', views.register, name='register')
-]
