f5e6fea957eaf13458f237259d0aed92942eeb29
DJZ <1601156268@qq.com>
2020-11-18 16:59:21 +0800

    example

--- school_app/urls.py
+++ school_app/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
                   path('', include('corecode.urls')),
                   path('student/', include('students.urls')),
                   path('staff/', include('staff.urls')),
+                  path('staffs/', include('staffs.urls')),
                   path('finance/', include('finance.urls')),
                   path('result/', include('result.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
