1e392ef20945da8956a2955ded67cd726dcf5e06
DJZ <1601156268@qq.com>
2020-11-20 21:49:14 +0800

    Day-2 Final

--- category/views.py
+++ category/views.py
@@ -22,15 +22,19 @@ class CategoryDetailView(LoginRequiredMixin, DetailView):
 
 
 class CategoryCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
-    model = CategoryForm
-    template_name = "category/category_form.html"
+    model = Category
+    fields = '__all__'
+    #template_name = "category/category_form.html"
     success_message = '新品类添加成功'
-    success_url = reverse_lazy('category-list')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = CategoryForm()
-        return context
+    def get_form(self):
+        '''add date picker in forms'''
+        form = super(CategoryCreateView, self).get_form()
+        return form
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['form'] = CategoryForm()
+    #     return context
 
 
 class CategoryUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
