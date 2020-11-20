b44704a303ea9d107d1dd32830ecc987cc3d8fbc
DJZ <1601156268@qq.com>
2020-11-20 21:17:57 +0800

    Day-2 Final

--- category/urls.py
+++ category/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category/list/', views.CategoryListView.as_view(), name='category-list'),
-    path('category/create/', views.CategoryCreateView.as_view(), name='category-create'),
-    path('category/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category-update'),
-    path('category/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category-delete'),
+    path('list/', views.CategoryListView.as_view(), name='category-list'),
+    path('create/', views.CategoryCreateView.as_view(), name='category-create'),
+    path('<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
+    path('<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category-update'),
+    path('<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category-delete'),
 ]
--- category/views.py
+++ category/views.py
@@ -1,49 +1,47 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, HttpResponseRedirect, redirect
-from django.contrib import messages
-from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView, ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.forms import widgets, forms
 from django.urls import reverse_lazy
 
 from .models import Category
-from .forms import CategoryForm
 
 
-class CategoryListView(LoginRequiredMixin, SuccessMessageMixin, ListView):
+class CategoryListView(LoginRequiredMixin, ListView):
     model = Category
-    #template_name = 'corecode/category_list.html'
+    context_object_name = 'category_list'
+    template_name = "category/category_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = CategoryForm()
-        return context
+
+class CategoryDetailView(LoginRequiredMixin, DetailView):
+    model = Category
+    template_name = "category/category_detail.html"
+    context_object_name = 'category'
 
 
 class CategoryCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
-    form_class = CategoryForm
-    #template_name = 'corecode/mgt_form.html'
-    success_url = reverse_lazy('category-list')
-    success_message = '品类信息创建成功'
+    model = Category
+    fields = '__all__'
+    success_message = '新品类添加成功'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = CategoryForm()
-        return context
+    def get_form(self):
+        '''add date picker in forms'''
+        form = super(CategoryCreateView, self).get_form()
+        return form
 
 
 class CategoryUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Category
-    fields = ['name', 'current']
-    success_url = reverse_lazy('category-list')
-    success_message = '品类信息更新成功'
-    #template_name = 'corecode/mgt_form.html'
+    fields = '__all__'
+    success_message = "品类信息修改成功."
+
+    def get_form(self):
+        '''add date picker in forms'''
+        form = super(CategoryUpdateView, self).get_form()
+        return form
 
 
 class CategoryDeleteView(LoginRequiredMixin, DeleteView):
     model = Category
     success_url = reverse_lazy('category-list')
-    #template_name = 'corecode/core_confirm_delete.html'
-    success_message = "品类信息删除成功"
-
