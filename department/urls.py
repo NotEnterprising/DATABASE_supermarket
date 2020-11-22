707a8127e628ff23957b00ba208e153f6668024d
litonghui <2686873626@qq.com>
2020-11-22 16:16:38 +0800

    staff_html

--- department/urls.py
+++ department/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('department/list/', views.DepartmentListView.as_view(), name='department-list'),
-    path('department/create/', views.DepartmentCreateView.as_view(), name='department-create'),
+    path('list/', views.DepartmentListView.as_view(), name='department-list'),
+    path('create/', views.DepartmentCreateView.as_view(), name='department-create'),
     path('<int:pk>/', views.DepartmentDetailView.as_view(), name='department-detail'),
-    path('department/<int:pk>/update/', views.DepartmentUpdateView.as_view(), name='department-update'),
-    path('department/<int:pk>/delete/', views.DepartmentDeleteView.as_view(), name='department-delete'),
+    path('<int:pk>/update/', views.DepartmentUpdateView.as_view(), name='department-update'),
+    path('<int:pk>/delete/', views.DepartmentDeleteView.as_view(), name='department-delete'),
 ]
