857bd44e944a825f2e9f1949e528cba29a8cfe83
DJZ <1601156268@qq.com>
2020-12-05 21:52:39 +0800

    Day-2 Final

--- staff/models.py
+++ staff/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.core.validators import RegexValidator
-from school_app.settings import hash_code
+
+from admin.models import User
+from department.models import Department
 from supermarket.models import Supermarket
 
 
@@ -12,25 +14,20 @@ class Staff(models.Model):
         ('female', '女')
     ]
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    USERNAME_FIELD = 'user.username'
+
     name = models.CharField(max_length=200, verbose_name='姓名')
     gender = models.CharField(max_length=10, choices=GENDER, default='男', verbose_name='性别')
     entry_date = models.DateField(default=timezone.now, verbose_name='入职日期')
-
-    username = models.CharField(max_length=200, verbose_name='用户名')
-    password = models.CharField(max_length=200, verbose_name='密码')
-
+    department = models.ForeignKey(Department, verbose_name='所属部门', on_delete=models.SET_NULL, blank=True, null=True)
     mobile_num_regex = RegexValidator(regex="^[0-9]{8,11}$", message="输入的电话号码格式不对")
     mobile_number = models.CharField(validators=[mobile_num_regex], max_length=13, blank=True, verbose_name='电话号码')
 
     address = models.TextField(blank=True, verbose_name='住址')
-
 
     def __str__(self):
         return f'{self.name}'
 
     def get_absolute_url(self):
         return reverse('staff-detail', kwargs={'pk': self.pk})
-
-    def save(self, *args, **kwargs):
-        self.password = hash_code(self.password)
-        super().save(*args, **kwargs)
--- staff/views.py
+++ staff/views.py
@@ -1,10 +1,14 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.forms import widgets, forms
 from django.urls import reverse_lazy
 
+from admin.models import User
+from .forms import StaffRegisterForm
 from .models import Staff
 
 
@@ -20,34 +24,75 @@ class StaffDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'staff'
 
 
-class StaffCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+class StaffDeleteView(LoginRequiredMixin, DeleteView):
     model = Staff
-    fields = '__all__'
-    success_message = '新员工添加成功'
+    success_url = reverse_lazy('staff-list')
 
-    def get_form(self):
-        '''add date picker in forms'''
-        form = super(StaffCreateView, self).get_form()
-        form.fields['entry_date'].widget = widgets.DateInput(
-            attrs={'type': 'date'})
+    def post(self, request, *args, **kwargs):
+        staff = Staff.objects.get(user_id=self.kwargs['pk'])
+        temp = staff.user
+        staff.delete()
+        temp.delete()
+        return redirect('staff-list')
+
+
+# @login_required()
+def register(request):
+    if request.method == 'POST':
+        form = StaffRegisterForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(username=form.cleaned_data['用户名'], password=form.cleaned_data['password1'],
+                                            is_staff=True)
+
+            staff = Staff.objects.create(name=form.cleaned_data['name'], gender=form.cleaned_data['gender'],
+                                         mobile_number=form.cleaned_data['mobile_number'],
+                                         address=form.cleaned_data['address'],
+                                         entry_date=form.cleaned_data['entry_date'], user_id=user.id)
+            staff.save()
+            messages.success(request, '新员工添加成功')
+            return redirect('staff-list')
         form.fields['address'].widget = widgets.Textarea(attrs={'rows': 1})
-        return form
+    else:
+        form = StaffRegisterForm()
+        form.fields['address'].widget = widgets.Textarea(attrs={'rows': 1})
+    return render(request, 'staff/staff_form.html', context={'form': form})
 
 
 class StaffUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Staff
-    fields = '__all__'
     success_message = "员工信息修改成功."
+    form_class = StaffRegisterForm
 
     def get_form(self):
         '''add date picker in forms'''
         form = super(StaffUpdateView, self).get_form()
+        form.fields['address'].widget = widgets.Textarea(attrs={'rows': 1})
         form.fields['entry_date'].widget = widgets.DateInput(
             attrs={'type': 'date'})
-        form.fields['address'].widget = widgets.Textarea(attrs={'rows': 1})
         return form
 
+    def post(self, request, *args, **kwargs):
+        form = StaffRegisterForm(request.POST)
+        if form.is_valid():
+            staff = Staff.objects.get(user_id=self.kwargs['pk'])
+            user = User.objects.get(id=self.kwargs['pk'])
+            staff.name = request.POST.get('name')
+            staff.gender = request.POST.get('gender')
+            staff.mobile_number = request.POST.get('mobile_number')
+            staff.address = request.POST.get('address')
+            staff.entry_date = request.POST.get('entry_date')
+            staff.save()
+            user.username = request.POST.get('用户名')
+            user.set_password(request.POST.get('password1'))
+            user.save()
+            return redirect('staff-list')
+        form.fields['address'].widget = widgets.Textarea(attrs={'rows': 1})
+        form.fields['entry_date'].widget = widgets.DateInput(
+            attrs={'type': 'date'})
+        return render(request, 'staff/staff_form.html', context={'form': form})
 
-class StaffDeleteView(LoginRequiredMixin, DeleteView):
-    model = Staff
-    success_url = reverse_lazy('staff-list')
+    def get(self, request, *args, **kwargs):
+        staff = Staff.objects.get(user_id=self.kwargs['pk'])
+        form = self.form_class(instance=staff)
+        form.fields['address'].widget = widgets.Textarea(attrs={'rows': 1})
+        return render(request, 'staff/staff_form.html', {'form': form})
