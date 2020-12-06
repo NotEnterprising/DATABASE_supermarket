5294e61e31660cdc0fc624e7dd216155e2ad3fc3
DJZ <1601156268@qq.com>
2020-12-06 17:19:36 +0800

    Day-2 Final

--- purchase/views.py
+++ purchase/views.py
@@ -3,28 +3,29 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render, HttpResponseRedirect, redirect
 
+from commodity.models import Commodity
 from customer.models import Customer
 from purchase.forms import CreatePurchase, EditPurchase
 from purchase.models import Purchase
 
 
-
 # Create your views here.
 @login_required
 def create_purchase(request):
+    commoditys = Commodity.objects.all()
     if request.method == 'POST':
         form = CreatePurchase(request.POST)
-        if form.is_valid():
-            commoditys = form.cleaned_data['commoditys']
+        commoditys = request.POST.getlist('commoditys')
+        if list:
             results = []
             customer = Customer.objects.get(user_id=request.user.id)
             for commodity in commoditys:
                 results.append(Purchase(commodity=commodity, customer=customer))
             Purchase.objects.bulk_create(results)
             return redirect('view-purchase')
-
-    form = CreatePurchase()
-    return render(request, 'create_purchase.html', {"form": form})
+        else:
+            messages.warning(request, "你没有选择任何商品")
+    return render(request, 'create_purchase.html', {"commoditys": commoditys})
 
 
 @login_required
